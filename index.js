87fa1224929b733ec759e87591d4fdb0271a8b64
Andrey Sitnik <andrey@sitnik.ru>
2017-10-21 22:21:36 +0200

    Add delay()

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-module.exports = { }
+/**
+ * Resolves a Promise after a specified amount of time.
+ *
+ * @param {number} delay Milliseconds to wait before resolving.
+ * @param {any} value Argument to be resolved by this Promise.
+ *
+ * @return {Promise} Promise which will be resolved after passed time.
+ *
+ * @example
+ * const delay = require('nanodelay')
+ *
+ * delay(300, "foo").then(result => {
+ *   // Executed after 300 milliseconds
+ *   result //=> "foo"
+ * })
+ *
+ * @name nanodelay
+ */
+module.exports = function (delay, value) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve(value)
+    }, delay)
+  })
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,16 @@
 var delay = require('../')
 
-it('exists', function () {
-  expect(typeof delay).toEqual('object')
+it('resolves after some time', function () {
+  var now = Date.now()
+  return delay(1000).then(function () {
+    var time = Date.now() - now
+    expect(time).toBeGreaterThanOrEqual(1000)
+    expect(time).toBeLessThan(1100)
+  })
+})
+
+it('resolves with specific result', function () {
+  return delay(1, 'foo').then(function (result) {
+    expect(result).toEqual('foo')
+  })
 })
