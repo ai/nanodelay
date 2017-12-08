219bc9ed1288018c9a87b109384223d5f24464e2
Danakt Frost <mail@danakt.ru>
2017-12-08 17:12:47 +0300

    Add typescript declaration (#3)
    * Add typescript typings
    
    * Import typescript typings

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+/**
+ * Resolves a Promise after a specified amount of time.
+ *
+ * @param {number} delay Milliseconds to wait before resolving.
+ * @param {any} value Argument to be resolved by this Promise.
+ *
+ * @return {Promise} Promise which will be resolved after passed time.
+ *
+ * @example
+ * import * as delay from 'nanodelay'
+ *
+ * delay(300, "foo").then(result => {
+ *   // Executed after 300 milliseconds
+ *   result //=> "foo"
+ * })
+ *
+ * @name nanodelay
+ */
+declare function delay(delay: number): Promise<void>;
+declare function delay<T>(delay: number, value: T): Promise<T>;
+
+declare module delay {}
+
+export = delay;
