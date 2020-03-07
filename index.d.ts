e735553ab8c1fa40c852eee742658500fb353b63
Andrey Sitnik <andrey@sitnik.ru>
2020-03-07 14:12:32 -0500

    Clean up JSDoc

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,17 @@
 /**
  * Resolves a Promise after a specified amount of time.
  *
- * @param {number} delay Milliseconds to wait before resolving.
- * @param {any} value Argument to be resolved by this Promise.
- *
- * @return {Promise} Promise which will be resolved after passed time.
- *
- * @example
+ * ```js
  * import * as delay from 'nanodelay'
  *
  * delay(300, "foo").then(result => {
  *   // Executed after 300 milliseconds
  *   result //=> "foo"
  * })
+ * ```
  *
- * @name nanodelay
+ * @param delay Milliseconds to wait before resolving.
+ * @param value Argument to be resolved by this Promise.
  */
 export function delay(delay: number): Promise<void>
 export function delay<T>(delay: number, value: T): Promise<T>
